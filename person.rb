23883841013c6fdd9d67939ad92e0e4aa29fe517
ichala <alijandhd@gmail.com>
2022-07-18 09:15:38 +0100

    Add person class

---
+++ person.rb
@@ -0,0 +1,23 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age, :permission
+
+  def initialize(age, permission = 'true', name = 'Unknown')
+    @id = Random.rand(1..500)
+    @permission = permission
+    @name = name
+    @age = age
+  end
+
+  def can_use_services?
+    return true if is_of_age? || permission
+
+    false
+  end
+
+  private
+
+  def is_of_age?
+    return true if @age >= 10
+  end
+end
