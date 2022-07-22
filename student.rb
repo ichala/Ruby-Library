dd40ae096eb6297aa365ee76c90195223dbcc03c
ichala <alijandhd@gmail.com>
2022-07-22 14:35:54 +0100

    Refactor methods

--- student.rb
+++ student.rb
@@ -1,17 +1,18 @@
-require './person'
+require_relative 'person'
 require_relative 'classroom'
+
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = Classroom.new(classroom)
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
   end
 
   def classroom=(classroom)
     @classroom = classroom
     classroom.students << self unless classroom.students.include?(self)
-  end
-
-  def play_hooky
-    "¯\(ツ)/¯"
   end
 end
