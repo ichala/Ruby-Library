59fb70c2bbd362dc1da59a6544d8176ae153a133
ichala <alijandhd@gmail.com>
2022-07-27 13:23:23 +0100

    Add remaining tests

---
+++ spec/rental_test.rb
@@ -0,0 +1,21 @@
+require './rental'
+require './student'
+require './book'
+
+require 'yaml'
+
+describe Rental do
+  before :each do
+    @book = Book.new('TEST', 'TEST')
+    @student = Student.new(13,23,'TEST')
+    @rental = Rental.new '03.02.24', @book, @student
+  end
+
+  it 'instance of Rental class' do
+    @rental.should be_an_instance_of Rental
+  end
+
+  it 'date is correct' do
+    @rental.date.should eql '03.02.24'
+  end
+end
